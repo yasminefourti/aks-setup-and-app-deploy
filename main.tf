ee70c3c87d4fe7b761a4454cd04e0a5dc7267f4f
yasminefourti <fourtiyasmine26@gmail.com>
2024-09-18 13:33:25 +0000

    add Helm release resource for Flux2 chart

--- main.tf
+++ main.tf
@@ -59,3 +59,12 @@ module "app" {
   rbac_aad                          = false
 
 }
+resource "helm_release" "flux" {
+  repository       = "https://fluxcd-community.github.io/helm-charts"
+  chart            = "flux2"
+  name             = "flux2"
+  namespace        = "flux-system"
+  create_namespace = true
+
+  depends_on = [module.app]
+}
